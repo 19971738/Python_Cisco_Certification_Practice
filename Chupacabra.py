7df4ff0b35a2af45579759856ab7cc77378e1750
Ponce Streaming <46233347+19971738@users.noreply.github.com>
2020-08-13 18:58:32 -0500

    Add files via upload

---
+++ Chupacabra.py
@@ -0,0 +1,11 @@
+secret_word = "chupacabra"
+
+while True:
+   word = input("Hallo, give me a word so you go out of the loop: ")
+   if word == "chupacabra":
+       break
+
+print("You've sucessfully gone out of the loop")
+print("Das ist einfach ein gute Stadt")
+
+   
